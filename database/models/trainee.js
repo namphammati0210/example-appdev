4f3a2dfdc992ce7231fb5654288aac84e0ee1138
Ngô Tiểu My <ngomy2001@Jades-MacBook-Pro.local>
2021-10-13 20:35:20 +0700

    create trainee

---
+++ database/models/trainee.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Trainee extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Trainee.init({
+    fullname: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    dateOfBirth: DataTypes.STRING,
+    education: DataTypes.STRING,
+    email: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Trainee',
+  });
+  return Trainee;
+};
---
+++ routes/trainingStaff.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var router = express.Router();
+const database = require("../database/models/index");
+const trainee = require("../database/models/trainee");
+const Role = database.db.Role;
+const Trainee = database.db.Trainee;
+const Account = database.db.Account;
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.send("hello training staff");
+});
+
+/* GET create trainee page. */
+router.get("/createTrainee", async function (req, res, next) {
+  const traineeRole = await Role.findOne({
+    where: {
+      name: "trainee",
+    },
+  });
+  res.render("trainee_view/create", { traineeRole: traineeRole });
+});
+
+router.post("/addTrainee", async function (req, res) {
+  const { username, password, fullname, age, dateOfBirth, education, email, roleId } =
+    req.body;
+
+  const trainee = await Trainee.create({
+    fullname,
+    age,
+    dateOfBirth,
+    education,
+    email,
+  });
+
+  if (trainee) {
+    await Account.create({
+      username,
+      password,
+      roleId,
+      userId: trainee.dataValues.id,
+    });
+
+    res.redirect("/trainingStaff");
+  }
+});
+
+module.exports = router;
+
