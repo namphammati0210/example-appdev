f5e7c7e670b92e73613b3b9bcac35dbe2a590e46
namphammati0210 <name.pham@io-asia.com>
2021-10-29 18:34:17 +0700

    feat: 🎸 Assign course to trainee

--- database/models/trainee.js
+++ database/models/trainee.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Trainee.hasMany(models.TraineeCourse, {
+        foreignKey: 'traineeId',
+        as: 'traineeId'
+      })
     }
   };
   Trainee.init({
